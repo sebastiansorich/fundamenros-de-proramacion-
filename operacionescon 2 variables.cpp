05d7a56c9df843bf057438905e29566b79e98530
sebastiansorich <69166184+sebastiansorich@users.noreply.github.com>
2020-08-17 17:06:46 -0400

    Add files via upload

---
+++ operacionescon 2 variables.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+//Sebastian Sorich//
+#include <iostream> 
+using namespace std; 
+int main()
+{
+    int a, b;
+    cout<< "Ingrese los numeros para realizar operaciones: " ;
+    cout << "Ingrese el primer numero: " ;
+    cin>> a;
+    cout << "Ingrese el segundo numero: " ;
+    cin>> b; 
+    cout << "La suma es: " << a+b;
+    
+    cout << "La resta: " << a-b; 
+    
+    cout << "La multiplicacion es: " << a*b;
+    
+    cout << "La division es: " << a/b;
+    
+    cout << "El resto es: " << a%b;
+
+    return 0;
+}
