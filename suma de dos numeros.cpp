2392b24ffbbfd1e48681349bb978e51bb8e6b089
sebastiansorich <69166184+sebastiansorich@users.noreply.github.com>
2020-08-17 17:47:30 -0400

    Add files via upload

---
+++ suma de dos numeros.cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+//sebastian Sorich//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int a, b, c ;
+   
+   cout<<"el primer sumando es:";
+   cin >> a;
+   cout<<"el segundo sumando es:";
+   cin >> b;
+   c = a+ b;
+   cout << "La suma es: " << c;
+
+    return 0;
+}
