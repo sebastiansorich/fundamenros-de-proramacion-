8599eae5d3cbd20b77361655d58536db3776da91
sebastiansorich <69166184+sebastiansorich@users.noreply.github.com>
2020-09-03 14:21:16 -0400

    Add files via upload

---
+++ valor absoluto .cpp
@@ -0,0 +1,22 @@
+//Sebastian Sorich 
+// valor absoluto 
+#include<iostream>
+
+using namespace std;
+int main()
+
+{
+float a
+
+	cout<<"Ingrese el numero del cual quiere saber el valor absoluto: ";
+	cin>>a;
+	if(a>=0)
+	{
+		cout<<"El valor absoluto es de "<<a;
+	}
+	else
+	{
+		cout<<"El valor absoluto es de "<<-a;
+	}
+	
+}
